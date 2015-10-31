7b9e5f5f8bcbc84f5892a532f27f42ebbf44b558
Sumit Bajracharya <sumeetbajra@gmail.com>
2015-10-31 18:35:38 +0545

    social plugin designed

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -3,7 +3,24 @@
 
   angular
     .module('portfolio')
-    .run(runBlock);
+    .run(runBlock)
+    .animation('.reveal-animation', function() {
+  return {
+    enter: function(element, done) {
+      element.css('display', 'none');
+      element.fadeIn(500, done);
+      return function() {
+        element.stop();
+      }
+    },
+    leave: function(element, done) {
+      element.fadeOut(500, done)
+      return function() {
+        element.stop();
+      }
+    }
+  }
+});
 
   /** @ngInject */
   function runBlock($log) {
